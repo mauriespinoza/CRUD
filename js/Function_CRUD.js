291db3f842befd48aac65988cd5003037bddab67
Mauricio Espinoza P <espinoza.p.mauricio@gmail.com>
2023-05-30 23:26:55 -0400

    Agrego dropdowns Familia Productos, se agregar input de busqueda

--- js/Function_CRUD.js
+++ js/Function_CRUD.js
@@ -1,6 +1,7 @@
 let products = []
 let idEditing = null;
 
+const familiaProd = document.getElementById('cmbFamiliaProd');
 
 const codProd = document.getElementById('txtCodProd');
 
@@ -14,9 +15,12 @@ const unitPriceProd = document.getElementById('txtUnitPriceProd');
 
 const table = document.getElementById('bodyTable');
 
+const search = document.getElementById('txtBuscar');
 
 readStorage();
 createTable();
+
+search.addEventListener('input',searchProducts);
 
 function addProduct(){
     //e.preventDefault()
@@ -27,6 +31,7 @@ function addProduct(){
         const index = products.findIndex((el) => el.id == idEditing);
         const product= {
             id,
+            familiaProd: familiaProd.value,
             codProd: codProd.value,
             nameProd: nameProd.value,
             descProd: descProd.value,
@@ -39,6 +44,7 @@ function addProduct(){
         console.log("not exits product");
         const product= {
               id,
+              familiaProd: familiaProd.value,
               codProd: codProd.value,
               nameProd: nameProd.value,
               descProd: descProd.value,
@@ -59,8 +65,10 @@ function saveStorage () {
 }
 
 function createTable(){
+   table.innerHTML ='';
     products.forEach( product => {
         table.innerHTML += `
+        <td>${product.familiaProd}</td>
         <td>${product.codProd}</td>
         <td>${product.nameProd}</td>
         <td>${product.descProd}</td>
@@ -75,16 +83,32 @@ function createTable(){
         `
       });
 }
+function searchTable(prod){
+  table.innerHTML ='';
+  prod.forEach( product => {
+       table.innerHTML += `
+       <td>${product.familiaProd}</td>
+       <td>${product.codProd}</td>
+       <td>${product.nameProd}</td>
+       <td>${product.descProd}</td>
+       <td>${product.stockProd}</td>
+       <td>${product.unitPriceProd}</td>
+       <td>
+         <button onclick="editProduct(${product.id})" class="btn btn-primary">Editar</button>
+       </td>
+       <td>
+         <button onclick="deleteProduct(${product.id})" class="btn btn-primary">Eliminar</button>
+       </td>
+       `
+     });
+}
 function editProduct(id){
   const index = products.findIndex((el) => el.id == id);
   idEditing = id;
   console.log("index: " + index);
   const product = products[index];
   console.log("product: " + product.id);
-  if(product){
-
-  }
-
+  familiaProd.value = product.familiaProd;
   codProd.value = product.codProd;
   nameProd.value = product.nameProd;
   descProd.value = product.descProd;
@@ -106,15 +130,33 @@ function readStorage(){
 }
 function deleteProduct(id){
     console.log(`>>>deleteProduct()`);
-    const index = products.findIndex((x) => x.id == id);
-
-    products.splice(index, 1);
-    clearForm();
-    saveStorage();
-    createTable();
+    let text = "Esta seguro de Eliminar el Producto!\nAceptar o Cancelar.";
+    if (confirm(text) == true) {
+      const index = products.findIndex((x) => x.id == id);
+      products.splice(index, 1);
+      clearForm();
+      saveStorage();
+      createTable();
+    } 
     console.log(`<<<deleteProduct()`);
 }
 function clearForm() {
     const form = document.getElementById('form');
     form.reset();
+}
+function searchProducts(e){
+  console.log(`>>>searchProducts()`);
+  const valor = search.value.toLowerCase(); 
+  table.innerHTML ='';
+  let resultProd = products.filter((prod) => prod.nameProd.toLowerCase().includes(valor));
+  if(resultProd.length>0){
+    searchTable(resultProd);
+  } else{
+    table.innerHTML = `
+        <div>
+        <h4>Sin resultados</h4> 
+        </div>
+        `;
   }
+  console.log(`<<<searchProducts()`);
+}
